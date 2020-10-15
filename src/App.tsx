d1d37a3da4fb15c7133bfe80ec63fbf531eb1d23
Matt Player <mattplayer@mac.com>
2020-10-15 12:27:45 +0100

    design completed but not componentised

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import image1 from "./images/desktop-image-hero-1.jpg"
+import mobimage1 from "./images/mobile-image-hero-1.jpg"
+import image2 from "./images/desktop-image-hero-2.jpg"
+import mobimage2 from "./images/mobile-image-hero-2.jpg"
+import image3 from "./images/desktop-image-hero-3.jpg"
+import mobimage3 from "./images/mobile-image-hero-3.jpg"
+import iconArrow from './images/icon-arrow.svg'
+import about1 from './images/image-about-dark.jpg'
+import about2 from './images/image-about-light.jpg'
+import hamburger from './images/icon-hamburger.svg'
 
+const fakeFetch = [
+  {title: "Discover innovative ways to decorate",
+body: "We provide unmatched quality, comfort and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.",
+imgUrl: image1,
+mobileImgUrl: mobimage1,
+},
+{title: "We are available all across the globe",
+body: "With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we're in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today.",
+imgUrl: image2,
+mobileImgUrl: mobimage2},
+{title: "Manufactured with the best materials",
+body: "Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office."}
+]
+
+const focusType = typeof fakeFetch[0]
 function App() {
+  const [currentFocus, setCurrentFocus]:[any, any] = useState(0)
+  const [menuExposed, setMenuExposed] = useState(false)
+  useEffect(() => {
+    setCurrentFocus(fakeFetch[0])
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <div className={`mask ${menuExposed ? "open" : ""}`}></div>
+        <nav className={`navbar ${menuExposed ? "navbar-open" : ""}`}>
+          <ul className="links">
+        <button onClick={() => setMenuExposed(!menuExposed)} className="link" aria-controls="menu-list" aria-expanded="false"></button>
+            <li className="link branding">
+              room
+            </li>
+            <li className="link collapsible">home</li>
+            <li className="link collapsible">shop</li>
+            <li className="link collapsible">about</li>
+            <li className="link collapsible">contact</li>
+          </ul>
+        </nav>
+        <div 
+        className="image slideshow-frame" 
+        style={{backgroundImage:`url(${currentFocus.imgUrl})`}}
         >
-          Learn React
-        </a>
-      </header>
+        </div>
+          <div className="slideshow-nav">
+            <button className="slideshow-nav-left slideshow-nav-button"></button>
+            <button className="slideshow-nav-right slideshow-nav-button"></button>
+          </div>
+        <div className="details-frame">
+          <h1>{currentFocus.title}</h1>
+          <p>{currentFocus.body}</p>
+          <a href="#" className="shop-now">
+            Shop Now
+            <img src={iconArrow} alt="Arrow"/>
+            </a>
+        </div>
+        <div className="image image-left footer-images" style={{backgroundImage: `url(${about1})`}}></div>
+        <div className="description-frame">
+          <h4>About Our Furniture</h4>
+          <p>Our multifunctional collection blends design and function to suit your individual tast. Make each room unique or pick a cohesive theme that best expresses your interests and what inspires you. Find the furniture pieces you need from traditional to contemporary styles or anything in between. Product specialists are available to help you create your dream space.</p>
+        </div>
+        <div className="image image-right footer-images" style={{backgroundImage: `url(${about2})`}}></div>
     </div>
   );
 }
